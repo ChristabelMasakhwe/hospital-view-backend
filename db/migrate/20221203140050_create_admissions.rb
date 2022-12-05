288cb1e3d5fae31fb46a39e871bfbb37447bcf96
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-12-05 10:04:29 +0300

    Create migrations

---
+++ db/migrate/20221203140050_create_admissions.rb
@@ -0,0 +1,11 @@
+class CreateAdmissions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admissions do |t|
+      t.datetime :date
+      t.integer :patient_id
+      t.integer :hospital_id
+
+      t.timestamps
+    end
+  end
+end
