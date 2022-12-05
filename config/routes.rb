54e565c02b16ab9ca5b5019b6ac1222894100c2e
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-12-05 10:05:19 +0300

    Add the needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :admissions, only: [:create]
+  resources :hospitals, only: [:index, :show, :update, :destroy]
+  resources :patients, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
